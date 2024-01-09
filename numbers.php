cb29a9dd94ed5af43f8635676882b0c56253a060
Anna Padun <roananik1988@gmail.com>
2024-01-09 21:23:54 +0100

    HW_7 while

---
+++ numbers.php
@@ -0,0 +1,24 @@
+<?php
+$num = 1;
+while ($num <= 10) {
+    echo $num . " ";
+    $num++;
+}
+echo "\n";
+
+
+
+$number = 5;
+$factorial = 1;
+while ($number > 1) {
+    $factorial *= $number;
+    $number--;
+}
+echo "factorial number 5: " . $factorial . "\n";
+
+
+$numb = 2;
+while ($numb <= 20) {
+    echo $numb . " ";
+    $numb += 2;
+}
